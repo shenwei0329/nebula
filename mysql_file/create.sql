6fb12a553a56c5c011ee61f3087ca5f3dc441179
shenwei <shenwei@1cc49945-5f95-451c-b81f-1e4cb698dbde>
2017-11-21 02:01:49 +0000

    增加表

--- mysql_file/create.sql
+++ mysql_file/create.sql
@@ -150,7 +150,7 @@ CREATE TABLE if not exists pd_group_member_t
 	id integer primary key not null auto_increment,
 	MEMBER_NAME varchar(128) comment '员工名',
 	GROUP_NAME varchar(128) comment '组名',
-	FLG INTEGER comment '标识：0、无效；1、有效'
+	FLG INTEGER comment '标识：0、无效；1、有效',
 	created_at datetime,
 	updated_at datetime
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
