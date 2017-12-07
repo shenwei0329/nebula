6ae028d9a87288da5e5473f055e8185fe91d0533
沈伟 <shenwei0329@hotmail.com>
2017-12-07 12:12:02 +0800

    完成程序
    完成程序，已实用。

--- python-proc/jira_fast_hdr.py
+++ python-proc/jira_fast_hdr.py
@@ -11,7 +11,29 @@ import types,json,sys,MySQLdb
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def inputFASTtask(cur, jira):
+def doSQLinsert(db,cur,_sql):
+    try:
+        cur.execute(_sql)
+        db.commit()
+    except:
+        db.rollback()
+
+def doSQLcount(cur,_sql):
+    try:
+        cur.execute(_sql)
+        _result = cur.fetchone()
+        _n = _result[0]
+        if _n is None:
+            _n = 0
+    except:
+        _n = 0
+    return _n
+
+def doSQL(cur,_sql):
+    cur.execute(_sql)
+    return cur.fetchall()
+
+def inputFASTtask(db, cur, jira):
     """
     从 JIRA 导入 FAST 任务数据
     :param cur: 数据库
@@ -35,9 +57,40 @@ def inputFASTtask(cur, jira):
             _s = _s.replace('[', '{"').replace(']', '"}').replace('=', '":"').replace(',', '","')
             _v = json.loads(_s)
 
-            Task[issue] = [issue.fields.summary, issue.fields.description.replace('\n', '').replace('\r', ''),
+            Task[issue] = [issue.id,
+                           issue.fields.summary,
+                           issue.fields.description.replace('\n', '').replace('\r', ''),
                            _v,
                            _user]
+
+    _keys = sorted(Task.keys(), reverse=True)
+    for _v in _keys:
+        _sql = 'select count(*) from jira_task_t where issue_id=%s' % Task[_v][0]
+        _n = doSQLcount(cur, _sql)
+        print _n
+        print _sql
+        if _n>0:
+            continue
+        else:
+            _sql = 'insert into jira_task_t(issue_id,summary,description,state,sequence,' \
+                   'stage_name,users,users_alias,user_emails,startDate,endDate,completeDate) ' \
+                   'values(%s,"%s","%s","%s",%s,"%s","%s","%s","%s","%s","%s","%s")' % (
+                Task[_v][0],
+                Task[_v][1],
+                Task[_v][2],
+                Task[_v][3]['state'],
+                Task[_v][3]['sequence'],
+                Task[_v][3]['name'],
+                Task[_v][4]['name'],
+                Task[_v][4]['alias'],
+                Task[_v][4]['email'],
+                Task[_v][3]['startDate'],
+                Task[_v][3]['endDate'],
+                Task[_v][3]['completeDate']
+            )
+            print _sql
+            doSQLinsert(db, cur, _sql)
+
     return Task
 
 if __name__ == '__main__':
@@ -49,12 +102,12 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="47.93.192.232",user="root",passwd="sw64419",db="nebula",charset='utf8')
     cur = db.cursor()
 
-    Task = inputFASTtask(cur, jira)
+    Task = inputFASTtask(db, cur, jira)
 
     _keys = sorted(Task.keys(),reverse=True)
     for _v in _keys:
-        print("\t%s, %s" % (Task[_v][0],Task[_v][1]))
-        for _k in sorted(Task[_v][2].keys(),reverse=True):
-            print("\t%s = %s" % (_k,Task[_v][2][_k]))
-        print("\tUser: %s, Alias: %s, eMail: %s" % (Task[_v][3]['name'],Task[_v][3]['alias'],Task[_v][3]['email']))
+        print("\t%s, %s" % (Task[_v][1],Task[_v][2]))
+        for _k in sorted(Task[_v][3].keys(),reverse=True):
+            print("\t%s = %s" % (_k,Task[_v][3][_k]))
+        print("\tUser: %s, Alias: %s, eMail: %s" % (Task[_v][4]['name'],Task[_v][4]['alias'],Task[_v][4]['email']))
         print("=" * 8)
