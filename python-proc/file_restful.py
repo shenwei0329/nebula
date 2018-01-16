e1b3370cea47c1b6becb757d1f5948ac66413164
沈伟 <shenwei0329@hotmail.com>
2018-01-16 14:10:18 +0800

    文件上传服务
    增加文件上传REST API服务file_restful.py。

--- python-proc/file_restful.py
+++ python-proc/file_restful.py
@@ -15,41 +15,22 @@ import os
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
-
-class TodoSimple(Resource):
-    def __init__(self):
-        try:
-            self.q = ipc.MessageQueue(19640419001, ipc.IPC_CREAT | ipc.IPC_EXCL)
-        except:
-            self.q = ipc.MessageQueue(19640419001)
-
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['filename']
-        self.q.send(_path, block=False)
-        return {todo_id: todos[todo_id]}
-
 @app.route('/upload', methods=['POST'])
 def upload():
     upload = request.files['file']
 
-    print upload
-
     if upload:
         filename = upload.filename
-
-        print filename
-
         upload.save(os.path.join('downloads', filename))
         app.logger.debug('File is saved as %s', filename)
+        try:
+            q = ipc.MessageQueue(19640419001, ipc.IPC_CREAT | ipc.IPC_EXCL)
+        except:
+            q = ipc.MessageQueue(19640419001)
+        q.send('downloads/%s' % filename, block=False)
+        return "Done"
 
-    return {"message": "OK"}
-
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
+    return "Error"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5010)
